69c525ec1d63c839ab5012a412de1117546d3a15
Maisarah <mamai29@test.com>
2023-04-10 15:45:55 +0800

    authorization

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var jwt = require('jsonwebtoken')
+
 
 let dbUsers=
 [
@@ -21,24 +23,38 @@ let dbUsers=
 },
 
 ]
+
 //enable json body parsing
 app.use(express.json());
 
+app.get('/hello',verifyToken, (req, res) => {
+    console.log(req.user)
+    res.send('Hello Wordl!')})
+
+//create a POST route for user to login
+//app.post('/login,(req,res')
+
+//app.post('/bye', (req, res)=>{
+  //  res.send('Bye Bye World!')})
+
+
+  
+
+
 app.post('/login', (req, res) => { 
     let data = req.body 
-    res.send(
-       login( data.username, data.password)
-    );
+    /*res.send(
+       login( data.username, data.password));*/
 
+       const user = login (data.username, data.password)
+        res.send(generateToken(user))
     });
 
     //post route for user to register
 app.post('/register', (req, res) => { 
         let data = req.body 
         res.send(
-           register( data.username, data.password, data.name, data.email)
-        );
-    
+           register( data.username, data.password, data.name, data.email));
         });
 
 
@@ -84,18 +100,39 @@ dbUsers.push
         return "success"
 
         }
-//app.get('/', (req, res) => {res.send('Hello Wordl!')})
 
-//create a POST route for user to login
-//app.post('/login,(req,res')
+//json web token
+function generateToken(userProfile)
+{
+    return jwt.sign({userProfile},'s7r0ng_p@5w0rd', {expiresIn: 60 * 60});
+}
+
+//to verify JWT token
+function verifyToken(req, res, next)
+{
+    let header = req.headers.authorization
+    console.log(header)
+
+    let token = header.split(' ')[1]
+
+    jwt.verify(token, 's7r0ng_p@5w0rd', function(err, decoded) {
+        
+
+    if (err) 
+        {   
+            res.send("Invalid Token") 
+        } 
+    
+        req.user = decoded
+        next()
+    })
+}
+    
+
+        //npm install express-jwt
+        //npm install bcryptjs
 
 
 
 
-//app.post('/bye', (req, res)=>{
-  //  res.send('Bye Bye World!')})
-
-
-//app.listen(port, () => {
-  //console.log(`Example app listening on port ${port}`)
         
